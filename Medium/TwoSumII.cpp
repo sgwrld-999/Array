9c311e15b818e5640fe5e43c417b88e0fc5a33c9
sgwrld-999 <130132265+sgwrld-999@users.noreply.github.com>
2024-10-11 14:49:16 +0530

    Commit

--- Medium/TwoSumII.cpp
+++ Medium/TwoSumII.cpp
@@ -4,13 +4,19 @@ using namespace std;
 Tracerse from both sides and check if the sum is equal to target then return the indices
 else if the sum is less than target then increment the left pointer
 else if the sum is greater than target then decrement the right pointer
-Time complexity : O(n)
+Time complexity : O(nlogn) // As we are traversing the array from both sides
 Space complexity : O(1) // This was the constraint in the question thtat constant should be used
 */
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
         int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            cout<<nums[i]<<" ";
+        }
+        cout<<endl;
         int i = 0 ;
         int j = n - 1 ;
         vector<int> ans(2);
@@ -31,9 +37,18 @@ public:
 
 int main(){
     Solution s;
-    vector<int> nums = {2,7,11,15};
-    int target = 9;
+    vector<int> nums = {2,7,11,15,1,5,6,3,1,6,7,8,235,345,7,235,6,0,1};
+    int target = 10;
     vector<int> ans = s.twoSum(nums,target);
     cout<<ans[0]<<" "<<ans[1]<<endl;
     return 0;
 }
+
+/*
+Test case:
+
+Input:
+nums = [2,7,11,15]
+target = 9
+
+*/
